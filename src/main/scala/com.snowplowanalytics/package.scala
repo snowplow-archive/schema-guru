db02dbab7336d1bf50a06bb08f1b238c45afc573
Alex Dean <alex@snowplowanalytics.com>
2014-11-22 11:19:15 +0000

    Cleaned up package structure /cc @jbeemster

--- src/main/scala/com.snowplowanalytics/package.scala
+++ src/main/scala/com.snowplowanalytics/package.scala
@@ -10,7 +10,7 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-package com.schema.guru
+package com.snowplowanalytics
 
 // Java
 import java.lang.{Integer => JInteger}
@@ -26,4 +26,4 @@ import Scalaz._
  * See:
  * http://www.artima.com/scalazine/articles/package_objects.html
  */
-package object common {}
+package object schemaguru {}
