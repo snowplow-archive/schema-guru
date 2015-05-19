f6efbd1d9b390363795dea49e7d4b5ddb5543a8a
Anton Parkhomenko <mailbox@chuwy.me>
2015-05-19 21:23:33 +0700

    Add reminder for duplicated keys in JSON
    Try to write specs2 test

--- src/test/scala/PropertyEnrichmentsSpecifications.scala
+++ src/test/scala/PropertyEnrichmentsSpecifications.scala
@@ -1,26 +1,32 @@
-import org.json4s.JsonAST.{JString, JObject}
-import org.scalacheck.Properties
-import org.scalacheck.Prop.{forAll, all}
+import org.specs2.{Specification, ScalaCheck}
+import org.json4s.JsonAST.{JObject, JString}
+import org.scalacheck.Prop.all
 
 import com.snowplowanalytics.schemaguru.generators.JsonSchemaGenerator.Enrichment
 
-object StringSpecification extends Properties("String") {
+
+class StringEnrichSpecification extends Specification with ScalaCheck  { def is = s2"""
+  Check string type enrichment
+    recognize ISO date                        $recognizeIsoDate
+    skip invalid date                         $doNotRecognizeIncorrectDate
+    add date-time format to string            $enrichFieldWithDate
+    """
 
   val correctDate = "2010-01-01T12:00:00+01:00"
   val incorrectDate = "2010-13-01T12:00:00+01:00"
-
   val StringT  = JObject(List(("type", JString("string"))))
   val StringWithDateT = JObject(List(("type", JString("string")), ("format", JString("date-time"))))
 
-  property("recognizeIsoDate") = all {
+
+  def recognizeIsoDate = all {
     Enrichment.validateDateTime(correctDate).isSuccess
   }
 
-  property("notRecognizeIncorrectDate") = all {
+  def doNotRecognizeIncorrectDate = all {
     Enrichment.validateDateTime(incorrectDate).isFailure
   }
 
-  property("enrichFieldWithDate") = all {
+  def enrichFieldWithDate = all {
     Enrichment.enrichString(correctDate) == StringWithDateT
   }
 }
