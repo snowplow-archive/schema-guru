d76c4fb2e4568d9e39ab8279a0b1a3ae4ca317cf
Anton Parkhomenko <mailbox@chuwy.me>
2015-05-28 21:04:42 +0700

    Fix unreduced product types, close #49

--- src/main/scala/com.snowplowanalytics/schemaguru/json/SchemaHelpers.scala
+++ src/main/scala/com.snowplowanalytics/schemaguru/json/SchemaHelpers.scala
@@ -3,6 +3,8 @@ package com.snowplowanalytics.schemaguru.json
 import scala.language.implicitConversions
 
 import org.json4s._
+import org.json4s.JsonDSL._
+import org.json4s.jackson.JsonMethods._
 
 object SchemaHelpers {
   /**
@@ -19,6 +21,22 @@ object SchemaHelpers {
 
   implicit def extractStringsFromJValues(jValues: List[JValue]): List[String] =
     for (jValue <- jValues; JString(str) <- jValue) yield str
+
+  implicit val formats = DefaultFormats // Brings in default date formats etc.
+
+  /**
+   * Some types in JSON Schema can be presented as product types
+   * e.g. "type": ["string", "number"].
+   * This predicate detects if current field contains type we're looking for
+   * @param value is possible value with JArray, JString or any other JValue
+   * @param string string we're looking for. Possible one of schema types
+   * @return whether string were found
+   */
+  def contains(value: JValue, string: String) = value match {
+    case JString(content) if (content == string) => true
+    case JArray(types) if types.contains(JString(string)) => true
+    case _ => false
+  }
 
   private case class Range(minimum: BigInt, maximum: BigInt)
 
@@ -55,6 +73,25 @@ object SchemaHelpers {
   }
 
   /**
+   * Tries to extract unreduced integer field
+   * Unreduced state imply it has minimum and maximum as arrays
+   *
+   * @param jField any JValue, but for extraction it need to be
+   *               JObject with type integer, and minimum/maximum arrays
+   * @return reducer if it is really integer field
+   */
+  private def extractIntegerField(jField: JValue): Option[IntegerFieldReducer] =  {
+    val list: List[IntegerFieldReducer] = for {
+      JObject(field) <- jField
+      JField("minimum", JArray(minimum)) <- field
+      JField("maximum", JArray(maximum)) <- field
+      JField("type", types) <- field
+      if (contains(types, "integer"))
+    } yield IntegerFieldReducer(minimum, maximum)
+    list.headOption
+  }
+
+  /**
    * Tries to extract all unreduced integer fields and modify minimum/maximum values
    *
    * @param original is unreduced JSON Schema with integer field
@@ -62,16 +99,11 @@ object SchemaHelpers {
    * @return JSON with reduced minimum/maximum properties
    */
   def reduceIntegerFieldRange(original: JValue) = {
-    val integerField: List[IntegerFieldReducer] = for { JObject(field) <- original
-                                                        JField("type", JString("integer")) <- field
-                                                        JField("minimum", JArray(minimum)) <- field
-                                                        JField("maximum", JArray(maximum)) <- field
-    } yield IntegerFieldReducer(minimum, maximum)
-
+    val integerField = extractIntegerField(original)
     integerField match {
-      case head :: Nil => original merge JObject("minimum" -> JInt(head.minimumBound),
-                                                 "maximum" -> JInt(head.maximumBound))
-      case _ => original
+      case Some(reducer) => original merge JObject("minimum" -> JInt(reducer.minimumBound),
+                                                   "maximum" -> JInt(reducer.maximumBound))
+      case None => original
     }
   }
 
@@ -85,6 +117,24 @@ object SchemaHelpers {
   }
 
   /**
+   * Tries to extract unreduced number field
+   * Unreduced state imply it has minimum field as array
+   *
+   * @param jField any JValue, but for extraction it need to be
+   *               JObject with type number, and minimum array
+   * @return reducer if it is really number field
+   */
+  private def extractNumberField(jField: JValue): Option[NumberFieldReducer] =  {
+    val list: List[NumberFieldReducer] = for {
+      JObject(field) <- jField
+      JField("minimum", JArray(minimum)) <- field
+      JField("type", types) <- field
+      if (contains(types, "number"))
+    } yield NumberFieldReducer(minimum)
+    list.headOption
+  }
+
+  /**
    * Eliminates maximum property possible left by merge with integer
    * and also minimum if number could be negative (otherwise set it to 0)
    *
@@ -92,17 +142,13 @@ object SchemaHelpers {
    *                 and minimum field as JArray in it
    */
   def reduceNumberFieldRange(original: JValue) = {
-    val numberField: List[NumberFieldReducer] = for { JObject(field) <- original
-                                               JField("type", JString("number")) <- field
-                                               JField("minimum", JArray(minimum)) <- field
-    } yield NumberFieldReducer(minimum)
-
+    val numberField = extractNumberField(original)
     numberField match {
-      case head :: Nil => original.merge(JObject("minimum" -> JInt(0)))  // it may be removed further
-                                  .removeField { case JField("minimum", _) => head.isNegative
-                                                 case JField("maximum", _) => true
-                                                 case _ => false }
-      case _ => original
+      case Some(reducer) => original.merge(JObject("minimum" -> JInt(0)))  // it may be removed further
+                                    .removeField { case JField("minimum", _) => reducer.isNegative
+                                                   case JField("maximum", _) => true
+                                                   case _ => false }
+      case None => original
     }
   }
 
@@ -114,6 +160,11 @@ object SchemaHelpers {
   def isMergedNumber(jArray: List[JValue]) =
     jArray.sorted(JValueOrdering.toScalaOrdering) == List(JString("integer"), JString("number"))
 
+  /**
+   * Holds information about merged JSON Schema String
+   *
+   * @param formats list of all encountered formats
+   */
   private case class StringFieldReducer(formats: List[String]) {
     def getFormat: String = {
       val formatSet = formats.toSet.toList
@@ -123,22 +174,36 @@ object SchemaHelpers {
   }
 
   /**
+   * Tries to extract unreduced string field
+   * Unreduced state imply it has format field as array
+   *
+   * @param jField any JValue, but for extraction it need to be
+   *               JObject with type string, and format array
+   * @return reducer if it is really string field
+   */
+  private def extractStringField(jField: JValue): Option[StringFieldReducer] =  {
+    val list: List[StringFieldReducer] = for {
+      JObject(field) <- jField
+      JField("format", JArray(formats)) <- field
+      JField("type", types) <- field
+      if (contains(types, "string"))
+    } yield StringFieldReducer(formats)
+    list.headOption
+  }
+
+  /**
    * Eliminates format property if more than one format presented
    *
    * @param original is unreduced JSON Schema with string field
    *                 and format property as JArray in it
    */
   def reduceStringFieldFormat(original: JValue) = {
-    val stringField: List[StringFieldReducer] = for { JObject(field) <- original
-                                                      JField("type", JString("string")) <- field
-                                                      JField("format", JArray(formats)) <- field
-    } yield StringFieldReducer(formats)
-
+    val stringField = extractStringField(original)
     stringField match {
-      case head :: Nil => original.merge(JObject("format" -> JString(head.getFormat)))  // it may be removed further
-                                  .removeField { case JField("format", JString("none")) => true
-                                                 case _ => false }
-      case _ => original
+      case Some(reducer) => original.merge(JObject("format" -> JString(reducer.getFormat)))  // it may be removed further
+                                    .removeField { case JField("format", JString("none")) => true
+                                                   case _ => false }
+      case None => original
     }
   }
 }
